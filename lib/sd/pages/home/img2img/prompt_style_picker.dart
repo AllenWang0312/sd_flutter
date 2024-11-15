32923c084f017b9ba5fc34be7c4b88989057cce3
Allen Wang <wangpengcheng02@dfwsgroup.com>
2024-11-15 18:03:49 +0800

    sc

--- lib/sd/pages/home/img2img/prompt_style_picker.dart
+++ lib/sd/pages/home/img2img/prompt_style_picker.dart
@@ -91,6 +91,12 @@ class SDPromptStylePicker extends StatelessWidget {
 //
 // =======
 
+    //一个女孩在咖啡馆里坐在座位上，身穿华丽的长裙
+      //A girl [3 sitting on] [1 a seat] [1 in a cafe], wearing a [gorgeous long dress]
+
+//Upper body support point  Lower body support point
+
+
       //槽位 0环境 1场景 2脸模 3体型 4衣服 5点缀 6辅助脸模 7辅助体态 8辅助装饰 9互动动作
       //10 11 12 脸模+衣服  13主体动作 14部位与衣物关系
       //todo 1. 10->12 3->13
